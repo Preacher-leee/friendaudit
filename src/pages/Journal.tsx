67177b3fda1f1aa04a51a4c913631037945d580b
Preacher-leee <travis.gordon727@gmail.com>
2025-05-12 22:31:59 -0400

    Create Journal.tsx

---
+++ src/pages/Journal.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Journal } from '../components/Journal';
+
+export const Journal = () => {
+  return (
+    <div className="min-h-screen pt-24 pb-20 px-4 bg-gradient-to-b from-black via-purple-950/10 to-black">
+      <div className="container mx-auto">
+        <Journal />
+      </div>
+    </div>
+  );
+};
