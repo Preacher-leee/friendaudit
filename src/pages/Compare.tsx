a13d37742a831e1a8dd48a48d8d15fc9c16ea75d
Preacher-leee <travis.gordon727@gmail.com>
2025-05-12 22:46:43 -0400

    Update Compare.tsx

--- src/pages/Compare.tsx
+++ src/pages/Compare.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { FriendComparison } from '../components/FriendComparison';
+import { compare } from '../components/compare';
 
-export const Compare = () => {
+export const compare = () => {
   return (
     <div className="min-h-screen pt-24 pb-20 px-4 bg-gradient-to-b from-black via-purple-950/10 to-black">
       <div className="container mx-auto">
-        <FriendComparison />
+        <compare />
       </div>
     </div>
   );
